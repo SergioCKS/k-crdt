f4f08456b44f64cb0c79825d9226c2e7372bb1c4
sergio-chumacero <39228828+sergio-chumacero@users.noreply.github.com>
2021-11-17 14:04:26 -0400

    path issue take 2

--- src/backend/wasm.ts
+++ src/backend/wasm.ts
@@ -3,7 +3,7 @@
  *
  * Interface to objects and methods from WASM linear memory.
  */
-import init, { Engine, UID, test_clock } from "./wasm/crdts";
+import init, { Engine, UID, test_clock } from "./wasm/crdts.js";
 
 /**
  * ## WASM
