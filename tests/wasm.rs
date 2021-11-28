924bfd270c5c5dce7be087cf9c310a0bdc681759
sergio-chumacero <39228828+sergio-chumacero@users.noreply.github.com>
2021-11-28 19:55:24 -0400

    server hlc wasm tests

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -17,7 +17,7 @@ use crdts::time::{
     client::{BrowserClock, BrowserHLC},
     clock::{test_clock, test_offsetted},
     hlc::{hlc_drift_is_limited, hlc_generate_timestamp_works, hlc_update_with_timestamp_works},
-    server::ServerClock,
+    server::{ServerClock, ServerHLC},
 };
 use wasm_bindgen_test::wasm_bindgen_test_configure;
 use wasm_bindgen_test::*;
@@ -25,17 +25,20 @@ use wasm_bindgen_test::*;
 // Configure wasm-pack to run in browser.
 wasm_bindgen_test_configure!(run_in_browser);
 
+// BrowserClock
 #[wasm_bindgen_test]
 fn browser_clock_works() {
     test_clock::<BrowserClock>();
     test_offsetted::<BrowserClock>();
 }
 
+// ServerClock
 #[wasm_bindgen_test]
 fn server_clock_works() {
     test_clock::<ServerClock>();
 }
 
+//#region BrowserHLC
 #[wasm_bindgen_test]
 fn browser_hlc_generate_timestamp_works() {
     hlc_generate_timestamp_works(BrowserHLC::default());
@@ -53,3 +56,18 @@ fn browser_hlc_drift_is_limited() {
     let hlc = BrowserHLC::default();
     hlc_drift_is_limited(hlc);
 }
+//#endregion
+
+//#region ServerHLC
+#[wasm_bindgen_test]
+fn server_hlc_generate_timestamp_works() {
+    hlc_generate_timestamp_works(ServerHLC::default());
+}
+
+#[wasm_bindgen_test]
+fn server_hlc_update_with_timestamp_works() {
+    let hlc1 = ServerHLC::default();
+    let hlc2 = ServerHLC::default();
+    hlc_update_with_timestamp_works(hlc1, hlc2);
+}
+//#endregion
