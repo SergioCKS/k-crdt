879c7e06862de5d9aeb873159b073f0d8a4bddb4
sergio-chumacero <39228828+sergio-chumacero@users.noreply.github.com>
2021-11-28 19:10:15 -0400

    binary convenience script for running wasm tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 //! # K-CRDT
 //!
 //! A library to manage a collection of CRDTS in a WASM context.
+#[cfg(feature = "client")]
+pub mod client;
 pub mod gcounter;
 pub mod lwwregister;
 pub mod pncounter;
+#[cfg(feature = "server")]
+pub mod server;
+pub mod steps;
 pub mod time;
 pub mod uid;
 pub mod vclock;
-#[cfg(feature = "client")]
-pub mod client;
-#[cfg(feature = "server")]
-pub mod server;
-#[cfg(all(not(feature = "client"), not(feature = "server")))]
-pub mod steps;
